fdc0104cefe424ed2d2a906673677a5d181df061
Habyb.Fernandes <habybfernandes@gmail.com>
2018-12-10 16:10:43 -0200

    Added: src Folder/Files

---
+++ MyMavenProject/src/main/java/com/elasticconcept/java/maven/Main.java
@@ -0,0 +1,11 @@
+package com.elasticconcept.java.maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// StringUtils.abbreviate(str, maxWidth)
+	}
+}
