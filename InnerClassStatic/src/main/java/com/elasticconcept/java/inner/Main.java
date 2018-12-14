5a755ecf2159175fadc8e1541005802bb22c501c
Habyb.Fernandes <habybfernandes@gmail.com>
2018-12-14 15:05:34 -0200

    Added: Inner Class Static Project

---
+++ InnerClassStatic/src/main/java/com/elasticconcept/java/inner/Main.java
@@ -0,0 +1,10 @@
+package com.elasticconcept.java.inner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		OuterClass.InnerClass c = new OuterClass.InnerClass();
+		c.print();
+	}
+}
---
+++ InnerClassStatic/src/main/java/com/elasticconcept/java/inner/OuterClass.java
@@ -0,0 +1,12 @@
+package com.elasticconcept.java.inner;
+
+public class OuterClass {
+
+	public static class InnerClass {
+		private int x = 10;
+		
+		public void print() {
+			System.out.println(x);
+		}
+	}
+}
